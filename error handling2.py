a1f5321c064cb69fdb604adfc5a748cbfc9bf011
Ontisitse Manyeneng <157315560+ONTI98@users.noreply.github.com>
2024-12-23 19:00:34 +0200

    Add files via upload

---
+++ error handling2.py
@@ -0,0 +1,32 @@
+#exception handling 
+#code to open and read file
+#should handle the 'file not found error'
+
+import sys
+#variables
+name=""
+filename=input("Please enter file name (C:\\Users\\User\\source\\repos):")
+READ='r'
+#open and read
+try:
+    with open (filename,mode=READ) as file:
+        print(file.read())
+
+except FileNotFoundError:
+    print(sys.exc_info()[0])  #you can throw an exception this if you do not know
+                              #the error
+
+
+                              #or you can simply test the programme,find the name
+                              #of the error  and place it after the except line
+
+
+
+
+
+
+       
+
+      
+
+
